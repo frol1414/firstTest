b92015855fed177f7eaf3076854ab5c43c58e41a
frol1414 <frol1414@gmail.com>
2020-10-29 17:52:06 +0300

    fix

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,42 +1,94 @@
 <template>
-  <div>
-    <p>FIRST</p>
-    <label for="email">Email</label>
-    <input type="text" id="email" v-model="email">
+  <section class="auth-container">
+    <div class="auth-tabs">
+      <a class="auth-tabs__item active">Авторизация</a>
+      <RouterLink to="/registration" class="auth-tabs__item">Регистрация</RouterLink>
+    </div>
+    <form>
+      <EvolAuthInput required label="Email" v-model="email" />
+      <EvolAuthInput required label="Пароль" v-model="password"/>
 
-    <label for="password">Password</label>
-    <input type="text" id="password" v-model="password">
+      <button type="button" class="btn btn-main auth-btn" @click="login">Авторизация</button>
 
-    <button @click="login">Log in</button>
-  </div>
+      <div class="login-social">
+        <p class="login-social__title">Войти в систему с</p>
+        <div id="uLogin" class="login-social__list">
+          <div class="login-social__item">
+            <a href="'vkontakte'">
+              <img :src="'img/icons/social/fb.png'" alt="">
+            </a>
+          </div>
+          <div class="login-social__item">
+            <a href="'vkontakte'">
+              <img :src="'img/icons/social/vk.png'" alt="">
+            </a>
+          </div>
+          <div class="login-social__item">
+            <a href="'google'">
+              <img :src="'img/icons/social/gp.png'" alt="">
+            </a>
+          </div>
+          <div class="login-social__item">
+            <a href="'mailru'">
+              <img :src="'img/icons/social/mr.png'" alt="">
+            </a>
+          </div>
+          <div class="login-social__item">
+            <a href="'yandex'">
+              <img :src="'img/icons/social/ydx.png'" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </form>
+  </section>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import EvolAuthInput from '@/components/EvolAuthInput';
+import { mapActions } from "vuex"
+
 export default {
-name: "login",
+  name: 'Login',
   data() {
     return {
       email: null,
       password: null,
     }
   },
-  mounted() {
-
-  },
   methods: {
     ...mapActions({
-      signIn: 'auth/signIn'
+      signIn: 'auth/signIn',
     }),
     async login() {
       console.log(111)
-      await this.signIn({email: this.email, password: this.password, device_name: 'tips'})
-
-    }
-  }
+      await this.signIn({ email: this.email, password: this.password, device_name: 'tips' })
+    },
+  },
+  components: { EvolAuthInput },
 }
 </script>
-
 <style scoped>
-
+.login-social__list {
+  display: flex;
+  justify-content: space-between;
+  width: 270px;
+  margin-bottom: 65px;
+}
+.login-social__title {
+  font-size: 17px;
+  line-height: 20px;
+  text-align: center;
+  color: #7D7D7D;
+  margin-bottom: 16px;
+}
+.login-social__item {
+  margin: 0 8px;
+  width: 38px;
+  height: 38px;
+}
+.login-social__item img {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/store/auth.js
+++ src/store/auth.js
@@ -1,56 +1,116 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
     namespaced: true,
 
     state: {
         authenticated: false,
-        user: null
+        user: null,
+        token: null,
     },
 
     getters: {
-        authenticated (state) {
-            return state.authenticated
+        authenticated(state) {
+            return state.authenticated;
         },
 
-        user (state) {
-            return state.user
+        user(state) {
+            return state.user;
+        },
+
+        getToken(state) {
+            return state.token;
         },
     },
 
     mutations: {
-        SET_AUTHENTICATED (state, value) {
-            state.authenticated = value
+        SET_AUTHENTICATED(state, value) {
+            state.authenticated = value;
         },
 
-        SET_USER (state, value) {
-            state.user = value
-        }
+        SET_USER(state, value) {
+            state.user = value;
+        },
+        SET_TOKEN(state, value) {
+            state.token = value;
+        },
     },
 
+    // actions: {
+    //     async signIn({ commit, dispatch }, credentials) {
+    //         console.log(credentials);
+    //         // await axios.get('/sanctum/csrf-cookie', {withCredentials: true})
+    //         await fetch(`https://api2.evolpay.ru/api/sanctum/token`, {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json;charset=utf-8',
+    //                 'Accept': 'application/json',
+    //             },
+    //             mode: 'no-cors',
+    //             body: JSON.stringify({
+    //                 ...credentials,
+    //             }),
+    //         })
+    //             .then((res) => console.log(res.json()))
+    //             .then((res) => commit('SET_TOKEN', res));
+    //
+    //         return dispatch('me');
+    //     },
+    //
+    //     me({ commit, state }) {
+    //         console.log('token', state.token)
+    //         return fetch('https://api2.evolpay.ru/api/user', {
+    //             headers: {
+    //                 'Content-Type': 'application/json;charset=utf-8',
+    //                 'Accept': 'application/json',
+    //                 'Authorization': `Bearer ${state.token}`,
+    //             },
+    //             mode: 'no-cors',
+    //         })
+    //             .then((response) => {
+    //                 commit('SET_AUTHENTICATED', true);
+    //                 commit('SET_USER', response.data);
+    //             })
+    //             .catch(() => {
+    //                 commit('SET_AUTHENTICATED', false);
+    //                 commit('SET_USER', null);
+    //             });
+    //     },
+    // },
     actions: {
-        async signIn ({dispatch}, credentials) {
-            console.log(credentials)
+        async signIn({ dispatch, commit }, credentials) {
+            console.log(credentials);
             // await axios.get('/sanctum/csrf-cookie', {withCredentials: true})
-            await axios.post('api/sanctum/token', credentials)
+            await axios.post('https://api2.evolpay.ru/api/sanctum/token', credentials)
+                .then((res) => commit('SET_TOKEN', res.data));
 
-            return dispatch('me')
+            return dispatch('me');
         },
 
-        async signOut ({ dispatch }) {
-            await axios.post('/logout')
+        async signOut({ dispatch }) {
+            await axios.post('/logout');
 
-            return dispatch('me')
+            return dispatch('me');
         },
 
-        me ({ commit }) {
-            return axios.get('/api/user').then((response) => {
-                commit('SET_AUTHENTICATED', true)
-                commit('SET_USER', response.data)
-            }).catch(() => {
-                commit('SET_AUTHENTICATED', false)
-                commit('SET_USER', null)
+        me({ commit, state }) {
+            console.log('token', state.token)
+            return axios.get('https://api2.evolpay.ru/api/user', {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${state.token}`,
+                },
+                mode: 'no-cors',
             })
-        }
-    }
-}
+                .then((response) => {
+                    commit('SET_AUTHENTICATED', true);
+                    commit('SET_USER', response.data);
+                })
+                .catch(() => {
+                    commit('SET_AUTHENTICATED', false);
+                    commit('SET_USER', null);
+                });
+        },
+    },
+};
